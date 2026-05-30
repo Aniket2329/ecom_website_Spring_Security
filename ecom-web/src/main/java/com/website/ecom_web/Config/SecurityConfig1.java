c26cc5596951a5d1dcfba0bbf53ecd32bc1ff456
Aniket2329 <aniketarya222@gmail.com>
2026-05-31 01:45:21 +0530

    fixed security config final

--- ecom-web/src/main/java/com/website/ecom_web/Config/SecurityConfig1.java
+++ ecom-web/src/main/java/com/website/ecom_web/Config/SecurityConfig1.java
@@ -1,3 +1,103 @@
+//package com.website.ecom_web.Config;
+//
+//import com.website.ecom_web.Filter.JwtAuthFilter;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Primary;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.authentication.AuthenticationProvider;
+//import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+//import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.CorsConfigurationSource;
+//
+//import java.util.List;
+//@EnableMethodSecurity
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig1 {
+//
+//    @Autowired
+//    private JwtAuthFilter jwtAuthFilter;
+//
+//    @Autowired
+//    private UserDetailsService UserDetailsService;
+//    @Primary
+//    @Bean
+//    public HttpSecurity securityFilterChain(HttpSecurity http) throws Exception {
+//
+//
+//        return http.csrf(customizer -> customizer.disable())
+//               // .cors(Customizer.withDefaults())
+//                .cors(cors -> cors.configurationSource(request -> {
+//                    CorsConfiguration config = new CorsConfiguration();
+//                    config.setAllowedOrigins(List.of(
+//                            "http://localhost:5173",
+//                            "https://current-frontend-eight.vercel.app"
+//                    ));
+//                    config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
+//                    config.setAllowedHeaders(List.of("*"));
+//                    config.setAllowCredentials(true);
+//                    return config;
+//                }))
+////                .authorizeHttpRequests(request -> request
+////                        // Public Endpoints
+////                        .requestMatchers("/api/auth/**").permitAll()// registration + login = public
+////                        .requestMatchers(HttpMethod.GET,"/api/products/**").permitAll()
+////
+////                        // Admin-only Endpoints
+////                        .requestMatchers("/api/products/add").hasRole("ADMIN")
+////                        .requestMatchers("/api/products/update/**").hasRole("ADMIN")
+////                        .requestMatchers("/api/products/delete/**").hasRole("ADMIN")
+////
+////                        // everything else = needs token
+////                        .anyRequest().authenticated())
+////                .sessionManagement(session ->
+////                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+////                .authenticationProvider(authenticationProvider())
+////                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+////                .build();
+//                .authorizeHttpRequests(request -> request
+//                        .requestMatchers("/api/auth/**").permitAll()
+//                        .requestMatchers(HttpMethod.GET, "/api/products/**").permitAll()
+//                        .requestMatchers("/api/products/add").hasRole("ADMIN")
+//                        .requestMatchers("/api/products/update/**").hasRole("ADMIN")
+//                        .requestMatchers("/api/products/delete/**").hasRole("ADMIN")
+//                        .anyRequest().authenticated()
+//                );
+//
+//    }
+//
+//    @Bean
+//    public AuthenticationProvider authenticationProvider(){
+//
+//        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+//        provider.setUserDetailsService(UserDetailsService);
+//        provider.setPasswordEncoder(new BCryptPasswordEncoder(12));
+//        return provider;
+//    }
+//
+//    @Bean
+//    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+//        return config.getAuthenticationManager();
+//    }
+//
+////    @Autowired
+////    private CorsConfigurationSource corsConfigurationSource;
+//}
+
+
 package com.website.ecom_web.Config;
 
 import com.website.ecom_web.Filter.JwtAuthFilter;
@@ -8,7 +108,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -18,8 +117,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-
 import java.util.List;
 
 @Configuration
@@ -34,10 +131,8 @@ public class SecurityConfig1 {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-
-        return http.csrf(customizer -> customizer.disable())
-               // .cors(Customizer.withDefaults())
+        return http
+                .csrf(csrf -> csrf.disable())
                 .cors(cors -> cors.configurationSource(request -> {
                     CorsConfiguration config = new CorsConfiguration();
                     config.setAllowedOrigins(List.of(
@@ -50,28 +145,22 @@ public class SecurityConfig1 {
                     return config;
                 }))
                 .authorizeHttpRequests(request -> request
-                        // Public Endpoints
-                        .requestMatchers("/api/auth/**").permitAll()// registration + login = public
-                        .requestMatchers(HttpMethod.GET,"/api/products/**").permitAll()
-
-                        // Admin-only Endpoints
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/products/**").permitAll()
                         .requestMatchers("/api/products/add").hasRole("ADMIN")
                         .requestMatchers("/api/products/update/**").hasRole("ADMIN")
                         .requestMatchers("/api/products/delete/**").hasRole("ADMIN")
-
-                        // everything else = needs token
-                        .anyRequest().authenticated())
+                        .anyRequest().authenticated()
+                )
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider())
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
-
     }
 
     @Bean
     public AuthenticationProvider authenticationProvider(){
-
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
         provider.setUserDetailsService(UserDetailsService);
         provider.setPasswordEncoder(new BCryptPasswordEncoder(12));
@@ -82,7 +171,4 @@ public class SecurityConfig1 {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
     }
-
-//    @Autowired
-//    private CorsConfigurationSource corsConfigurationSource;
 }
